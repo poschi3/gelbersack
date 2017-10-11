156f7075fee010811539614d6f83e75d9d5add8e
Poschi <development@poschi.net>
2017-10-11 21:05:56 +0200

    Changed reading input file, date is now from 4 to 10, umlauts working

--- gelbersacknbg.py
+++ gelbersacknbg.py
@@ -9,10 +9,7 @@ import os, pytz, uuid
 import datetime
 from icalendar import Calendar, Event, vText
 
-# Parameter parsen:
-
 filesToDo = []
-
 for dirname, dirnames, filenames in os.walk('.'):
     for filename in filenames:
         if filename.endswith(".year"):
@@ -22,73 +19,57 @@ for dirname, dirnames, filenames in os.walk('.'):
 print filesToDo
 
 #Werte aus Datei laden:
-
-
-dates = [[] for x in xrange(10)] 
-
-
-# Datei Auslesen
+dates = [[] for x in xrange(10)]
 for file in filesToDo:
     year = int(file[:-5])
     print year
     lines = open(file, "rU")
-    players = []
-    
+
     route = 0
     month = 0
 
-    # F�r jede Zeile in der Datei
+    # Für jede Zeile in der Datei
     for days in lines:
-        for day in days.split("/"): # Eine Zeile enth�lt 1 bis 4 Termine
+        for day in days.split("/"): # Eine Zeile enthält 1 bis 4 Termine
             realDay = int(day.strip()[:-1])
             realMonth = int(month + 1)
+            #print str(year) + "-" + str(realMonth) + "-" + str(realDay) + " Route " + str(route)
             datum = datetime.date(int(year), realMonth, realDay)
             datum = datetime.datetime(year=int(year), month=realMonth, day=realDay, tzinfo=pytz.timezone("Europe/Berlin"))
             dates[route].append(datum)
 
-        route += 1
-        if route >= 10:
-            month += 1
-            route = 0
+        month += 1
+        if month >= 12:
+            route += 1
+            month = 0
 
 # Pro Route ausgeben:
-
 routeNum = 1
 for route in dates:
-    
     cal = Calendar()
     cal.add("prodid", "python")
     cal.add("version", "2.0")
-    cal["X-WR-CALNAME"] = "Gelber Sack Nuernberg Route " + str(routeNum)
-    cal["X-WR-CALDESC"] = "Abfuhrplan fuer den gelben Sack in Nuernberg von 2014 bis 2016 fuer die Route " + str(routeNum)
+    cal["X-WR-CALNAME"] = "Gelber Sack Nürnberg Route " + str(routeNum)
+    cal["X-WR-CALDESC"] = "Abfuhrplan für den gelben Sack in Nürnberg von 2017 bis 2019 für die Route " + str(routeNum)
     
     for date in route:
-        nextDay = date + datetime.timedelta(days=1)
+        #nextDay = date + datetime.timedelta(days=1)
+        start = datetime.datetime.combine(date, datetime.time(4, 0))
+        end = datetime.datetime.combine(date, datetime.time(10, 0))
+
         event = Event()
         event.add("summary", "Gelber Sack")
-        event.add("dtstart", date)
-        event.add("dtend", nextDay)
+        event.add("dtstart", start)
+        event.add("dtend", end)
         event.add("dtstamp", date)
         
-        #event['uid'] = uuid.uuid4()
+        event['uid'] = uuid.uuid4()
         event['location'] = vText("Route " + str(routeNum))
         event['description'] = vText("Route " + str(routeNum))
         
         cal.add_component(event)
         
-    #directory = tempfile.mkdtemp()
-    #print directory
-    
     f = open("route" + str(routeNum) + ".ics", "wb")
     f.write(cal.to_ical())
     f.close()
-#     
     routeNum += 1
-    
-    
-        
-
-
-
-
-
